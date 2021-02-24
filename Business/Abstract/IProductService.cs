b65de733438f71d19bd300dff363ec5447094e83
ilknur sercek <ilknursercek4@gmail.com>
2021-02-25 00:18:06 +0300

    24 Şubat ders sonu

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -16,6 +16,7 @@ namespace Business.Abstract
         IDataResult<List<ProductDetailDto>> GetProductDetails();
         IDataResult<Product> GetById(int productID); //Data olduğu için IDataResult
         IResult Add(Product product); //Data olmadığı için IResult
+        IResult Update(Product product);
         
     }
 }
